fefc67a50d7d88f155924fa04cf9d4cae9b39e9e
GBstallman <nduwimanaguymich@gmail.com>
2021-03-20 14:18:14 +0200

    changed models

--- api/models.py
+++ api/models.py
@@ -13,39 +13,15 @@ class Actor(models.Model):
 		return f"{self.user.first_name} {self.user.last_name}"
 
 
-class Adress_distributor(models.Model):
+class AdressDistributor(models.Model):
 	province = models.CharField(max_length=30)
 	commune = models.CharField(max_length=30)
 	colline = models.CharField(max_length=20)
 	phone_number = models.PositiveIntegerField()
-	distributor = models.OneToOneField(Actor,on_delete=models.CASCADE)
+	distributor = models.OneToOneField(User,on_delete=models.CASCADE)
 
 	def __str__(self):
 		return f'{self.province} {self.commune} {self.colline} {self.phone_number}'
-
-
-class SeedCategory(models.Model):
-	SEED_CHOICES = (
-		('Souches', 'Souches'),
-		('Pré_Bases', 'Pré_Bases'),
-		('Base', 'Base'),
-		('Cerifiés', 'Cerifiés'),
-		)
-	nom = models.CharField(max_length=30,choices=SEED_CHOICES, verbose_name='Category')
-
-	def __str__(self):
-		return self.nom
-
-	class Meta:
-		verbose_name = 'Seed Category'
-		verbose_name_plural = 'Seed Categories'
- 
-
-class RateSeed(models.Model):
- 	validé = models.BooleanField(null=False)
- 	actor = models.ForeignKey(Actor,on_delete=models.CASCADE)
- 	def __str__(self):
- 		return f" {self.actor} {self.status}"
 
 
 class Plant(models.Model):
@@ -59,7 +35,7 @@ class Plant(models.Model):
         ordering = ["nom"]
 
 class Client(models.Model):
-    nom = models.OneToOneField(User, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=30)
     tel = models.CharField(max_length=24)
 
     class Meta:
@@ -78,23 +54,29 @@ class Variety(models.Model):
 
 
 class Seed(models.Model):
+
+	SEED_CHOICES = (
+		('Souches', 'Souches'),
+		('Pré_Bases', 'Pré_Bases'),
+		('Base', 'Base'),
+		('Cerifiés', 'Cerifiés'),
+		)
+	category = models.CharField(max_length=30,choices=SEED_CHOICES, verbose_name='Category')
 	nom = models.OneToOneField(Plant,on_delete=models.PROTECT)
-	rate = models.OneToOneField(RateSeed,on_delete=models.CASCADE)
 	prix = models.PositiveIntegerField()
 	disponible = models.BooleanField(default=True)
-	exp_date = models.DateField(editable=True, null=False)
+	photo = models.FileField()
 	etat_sanitaire = models.TextField(max_length=30)
 	variety    = models.ForeignKey(Variety, on_delete=models.PROTECT)
 
 
 	def __str__(self):
-		return  f"{self.nom} {self.prix} {self.disponible} {self.quantite} {self.etat_sanitaire} {self.variety} "
+		return  f"{self.nom} {self.category} {self.prix} {self.disponible} {self.quantite} {self.etat_sanitaire} {self.variety} "
 
 
 class Stock(models.Model):
-    plant = models.ForeignKey(
-        Plant, default=None, on_delete=models.CASCADE)
-    lot = models.CharField(max_length=30)
+    seed = models.ForeignKey(
+        Seed, default=None, on_delete=models.CASCADE)
     quantite_initiale = models.FloatField(
         default=None, verbose_name='quantité initial')
     quantite_actuelle = models.FloatField(
@@ -103,7 +85,7 @@ class Stock(models.Model):
 
 
     def __str__(self):
-        return f"{self.plant} {self.quantite_actuelle} du {self.date}"
+        return f"{self.quantite_actuelle} du {self.date}"
 
 class Commande(models.Model):
     actor = models.ForeignKey(
